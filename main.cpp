dc7d3daa1fabe3bb2defb210325be8c82ae42e6f
demiurg03 <demiurg03@users.noreply.github.com>
2019-05-19 01:05:55 +0700

    19.05.2019

--- main.cpp
+++ main.cpp
@@ -7,10 +7,13 @@ int main()
     //shape.setFillColor(sf::Color::Green);
 
     sf::Texture tPlayer;
-    tPlayer.loadFromFile("entity.png");
+    tPlayer.loadFromFile("test_entity.png");
+
+    float fPlayer=0;
 
     sf::Sprite sPlayer;
     sPlayer.setTexture(tPlayer);
+    sPlayer.setTextureRect( sf::IntRect(0,0,50,50) );
     sPlayer.setPosition(50,100);
 
     while (window.isOpen())
@@ -20,6 +23,23 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            sPlayer.move(-0.1,0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            sPlayer.move(0.1,0);
+
+            fPlayer += 0.5;
+        if (fPlayer > 8) fPlayer -=8 ;
+
+            sPlayer.setTextureRect( sf::IntRect(0*int(fPlayer),0,50,50) );
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            sPlayer.move(0,-0.1);
         }
 
         window.clear();
