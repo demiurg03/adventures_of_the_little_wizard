274833c992e3b782b832389ed28cf939ffcad51a
demiurg03 <demiurg03@users.noreply.github.com>
2019-06-28 20:36:34 +0700

    28.06.2019
    added gravity system

--- main.cpp
+++ main.cpp
@@ -1,52 +1,123 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+static sf::Sprite oWall;
+
+
+
+
+
+
+class CPlayer{
+
+float width,height;
+float x,y;
+
+sf::Sprite spritePlayer;
+sf::Texture texturePlayer;
+
+
+public:
+
+CPlayer(){
+texturePlayer.loadFromFile("entity.png");
+spritePlayer.setTexture(texturePlayer);
+spritePlayer.setPosition(0,100);
+
+x = spritePlayer.getPosition().x;
+y = spritePlayer.getPosition().y;
+
+width = spritePlayer.getGlobalBounds().width;
+height = spritePlayer.getGlobalBounds().height;
+
+}
+
+
+void update(){
+float _width,_height;
+float _x,_y;
+
+
+_x = oWall.getPosition().x;
+_y= oWall.getPosition().y;
+
+_width = oWall.getGlobalBounds().width;
+_height = oWall.getGlobalBounds().height;
+
+
+
+if(!(x <= _x+_width&& x+width >= _x && y <= _y+_height &&y+height >= _y)){
+y+=0.1f;
+}
+else{
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+        y-=100;
+    }
+}
+
+
+
+
+if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+x+=0.1f;
+}
+
+if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+x-=0.1f;
+}
+
+
+
+
+spritePlayer.setPosition(x,y);
+
+}
+
+void draw(sf::RenderWindow& window){
+window.draw(spritePlayer);
+}
+
+
+sf::Texture getTexture() const
+{
+return texturePlayer;
+}
+};
+
+
+
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1280,665), "Adventures of the Little Wizard");
-    //sf::CircleShape shape(100.f);
-    //shape.setFillColor(sf::Color::Green);
+sf::RenderWindow window(sf::VideoMode(1280,665),"Adventures of the Little Wizard");
 
-    sf::Texture tPlayer;
-    tPlayer.loadFromFile("test_entity.png");
+CPlayer player;
 
-    float fPlayer=0;
 
-    sf::Sprite sPlayer;
-    sPlayer.setTexture(tPlayer);
-    sPlayer.setTextureRect( sf::IntRect(0,0,50,50) );
-    sPlayer.setPosition(50,100);
+oWall.setTexture(player.getTexture());
+oWall.setPosition(0,480);
+oWall.setTextureRect(sf::IntRect(0,0,1280,50));
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        {
-            sPlayer.move(-0.1,0);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        {
-            sPlayer.move(0.1,0);
 
-            fPlayer += 0.5;
-        if (fPlayer > 8) fPlayer -=8 ;
 
-            sPlayer.setTextureRect( sf::IntRect(0*int(fPlayer),0,50,50) );
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        {
-            sPlayer.move(0,-0.1);
-        }
 
-        window.clear();
-        window.draw(sPlayer);
-        window.display();
-    }
-
-    return 0;
+while (window.isOpen())
+{
+sf::Event event;
+while (window.pollEvent(event))
+{
+if (event.type == sf::Event::Closed)
+window.close();
 }
-//git
+
+window.clear();
+
+player.update();
+player.draw(window);
+
+window.draw(oWall);
+window.display();
+}
+
+return 0;
+}
